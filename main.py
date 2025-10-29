26a6593b9edddd57904e99db1137498650c88b3f
Sergio Bonilla <shidoxxthoka@gmail.com>
2025-10-29 18:55:17 -0500

    main_act_2

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from typing import List, Optional
 
 from DB import obtener_bd, motor
 from modelos import Base
-from esquemas import Estudiante, EstudianteCrear, EstudianteActualizar
+from esquemas import Estudiante, EstudianteCrear, EstudianteActualizar,Curso, CursoCrear, CursoActualizar, CursoConEstudiantes
+
 import crud
 
 # Crear tablas
@@ -31,6 +32,40 @@ def actualizar_estudiante(estudiante_id: int, estudiante: EstudianteActualizar, 
 @app.delete("/estudiantes/{estudiante_id}")
 def eliminar_estudiante(estudiante_id: int, bd: Session = Depends(obtener_bd)):
     return crud.eliminar_estudiante(bd=bd, estudiante_id=estudiante_id)
+
+# Endpoints para Cursos
+@app.post("/cursos/", response_model=Curso, status_code=status.HTTP_201_CREATED)
+def crear_curso(curso: CursoCrear, bd: Session = Depends(obtener_bd)):
+    return crud.crear_curso(bd=bd, curso=curso)
+
+@app.get("/cursos/", response_model=List[Curso])
+def leer_cursos(
+    creditos: Optional[int] = None, 
+    codigo: Optional[str] = None, 
+    bd: Session = Depends(obtener_bd)
+):
+    return crud.obtener_cursos_creditos_codigo(bd=bd, creditos=creditos, codigo=codigo)
+
+@app.get("/cursos/{curso_id}", response_model=CursoConEstudiantes)
+def leer_curso(curso_id: int, bd: Session = Depends(obtener_bd)):
+    return crud.obtener_curso_id(bd=bd, curso_id=curso_id)
+
+@app.put("/cursos/{curso_id}", response_model=Curso)
+def actualizar_curso(curso_id: int, curso: CursoActualizar, bd: Session = Depends(obtener_bd)):
+    return crud.actualizar_curso(bd=bd, curso_id=curso_id, curso_actualizar=curso)
+
+@app.delete("/cursos/{curso_id}")
+def eliminar_curso(curso_id: int, bd: Session = Depends(obtener_bd)):
+    return crud.eliminar_curso(bd=bd, curso_id=curso_id)
+
+@app.get("/estudiantes/{estudiante_id}/cursos", response_model=List[Curso])
+def obtener_cursos_estudiante(estudiante_id: int, bd: Session = Depends(obtener_bd)):
+    return crud.obtener_cursos_estudiante(bd=bd, estudiante_id=estudiante_id)
+
+@app.get("/cursos/{curso_id}/estudiantes", response_model=List[Estudiante])
+def obtener_estudiantes_curso(curso_id: int, bd: Session = Depends(obtener_bd)):
+    return crud.obtener_estudiantes_curso(bd=bd, curso_id=curso_id)
+
 
 # raíz
 @app.get("/")
